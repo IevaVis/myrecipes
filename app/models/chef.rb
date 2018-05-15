1164d699c454a1712b5103b1f66a8e03107e7aa6
IevaVis <ieva.visniauskiene@gmail.com>
2018-05-15 14:15:02 +0800

    added one to many associations between recipe and chef

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -5,4 +5,5 @@ class Chef < ApplicationRecord
 	validates :chefname, length: { maximum: 30 }
 	validates :email, uniqueness: {case_sensitive: false}
 	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+	has_many :recipes
 end
