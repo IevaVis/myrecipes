83e5cf8299cb987e947f2b2b25e5c3988f2c777e
IevaVis <ieva.visniauskiene@gmail.com>
2018-05-15 13:14:35 +0800

    added recipe and chef models with tests

---
+++ app/models/chef.rb
@@ -0,0 +1,8 @@
+class Chef < ApplicationRecord
+	has_secure_password
+	before_save {email.downcase!}
+	validates :chefname, :email, :password, :password_confirmation, presence: true
+	validates :chefname, length: { maximum: 30 }
+	validates :email, uniqueness: {case_sensitive: false}
+	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+end
---
+++ test/models/chef_test.rb
@@ -0,0 +1,57 @@
+require 'test_helper'
+class ChefTest < ActiveSupport::TestCase
+
+	def setup
+		@chef = Chef.new(chefname: "Ieva", email: "ieva@example.com", password: "12345", password_confirmation: "12345")
+	end
+
+	test "should be valid" do
+		assert @chef.valid?		
+	end
+
+	test "name should be present" do
+		@chef.chefname = " "
+		assert_not @chef.valid?
+	end
+
+	test "email should be present" do
+		@chef.email = " "
+		assert_not @chef.valid?
+	end
+
+	test "password should be present" do
+		@chef.password = " "
+		assert_not @chef.valid?
+	end
+
+	test "password confirmation should be valid" do
+		@chef.password_confirmation = " "
+		assert_not @chef.valid?
+	end
+
+	test "name should be less than 30 characters" do
+		@chef.chefname = "a" * 31
+		assert_not @chef.valid?
+	end
+
+	test "email should be of correct format" do
+		@chef.email = "email.email.com"
+		assert_not @chef.valid?
+	end
+
+	test "email should be unique and case insensitive" do
+		duplicate_chef = @chef.dup
+		duplicate_chef.email = @chef.email.upcase
+		@chef.save
+		assert_not duplicate_chef.valid?
+	end
+
+	test "email should be saved as lowercase" do
+		mixed_case_email = "IeVa@EXample.COM"
+		@chef.email = mixed_case_email
+		@chef.save
+		assert_equal mixed_case_email.downcase, @chef.reload.email
+	end
+
+end
+
