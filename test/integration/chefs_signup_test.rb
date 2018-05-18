c7b0f02aaad71e777e2d811e9d827ca897b7781e
IevaVis <ieva.visniauskiene@gmail.com>
2018-05-18 10:21:42 +0800

    chef signup test

--- test/integration/chefs_signup_test.rb
+++ test/integration/chefs_signup_test.rb
@@ -8,8 +8,25 @@ class ChefsSignupTest < ActionDispatch::IntegrationTest
  end
 
  test "reject an invalid signup" do
+ 	get signup_path
+ 	assert_template 'chefs/new'
+ 	assert_no_difference "Chef.count" do
+ 		post chefs_path, params: { chef: { chefname: " ", email: " ", password: " ", password_confirmation: " "} }
+ 	end
+ 	assert_template 'chefs/new'
+ 	assert_select 'h6.card-title'
+ 	assert_select 'div.card-body'
+
  end
 
  test "accept valid signup" do
+ 	get signup_path
+ 	assert_template 'chefs/new'
+ 	assert_difference "Chef.count", 1 do
+ 		post chefs_path, params: { chef: { chefname: "Ieva", email: "ieva@example.com", password: "12345", password_confirmation: "12345"} }
+ 	end
+ 	follow_redirect!
+ 	assert_template 'chefs/show'
+ 	assert_not flash.empty?
  end
 end
