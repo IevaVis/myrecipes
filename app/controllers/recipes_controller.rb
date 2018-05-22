74b3e4996966cc93939907e66c484b1ed5ae1503
IevaVis <ieva.visniauskiene@gmail.com>
2018-05-22 19:55:04 +0800

    added comments

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -23,6 +23,7 @@ class RecipesController < ApplicationController
 	end
 
 	def show
+		@comments = @recipe.comments.paginate(page: params[:page], per_page: 5).order('created_at DESC')
 	end
 
 	def edit
